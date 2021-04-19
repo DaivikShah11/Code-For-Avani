dfe1f4a18a77905f69973c7a5a0633164d456583
DaivikShah11 <74413571+DaivikShah11@users.noreply.github.com>
2021-04-19 19:05:42 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var canvas,backgroungImage;
+var gamestate=0;
+var playercount;
+var database;
+var form,player,game;
+
+
+
+function setup(){
+  database = firebase.database();
+  canvas =  createCanvas(500,500);
+
+}
+
+function draw(){
+ 
+}
+
+
